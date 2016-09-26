ee1d6e402b96463898d931530fdd1208292ab157
Damon Earp <earp.dn@gmail.com>
2016-09-26 16:37:03 -0400

    updated comments and fixed an error with the symlinkTarget attribute in io_entry.c

--- src/io/io_xattr.h
+++ src/io/io_xattr.h
@@ -5,16 +5,14 @@
  *
  * @section DESCRIPTION
  *
- * API for reading and writing entries from and to a stream. Entries are written
- * as JSON Objects where only valid digests are written. A single entry is on
- * a single line.
+ * API for reading and writing extended attributes to a stream. Each attribute
+ * is written as a JSON Object with the path's md5.
  */
 #ifndef IO_XATTR_H__
 #define IO_XATTR_H__
 
 #include <stdio.h>
 #include <stddef.h>
-#include <time.h>
 #include <sys/types.h>
 
 /* Public API *****************************************************************/
@@ -24,9 +22,9 @@
  * write the following fields as a JSON object to the stream
  *
  * @param pathmd5       16 byte md5 of the path
- * @param name			the name of the xattr field
- * @param valuebuffer	the contents of the xattr field
- * @param valuesize		the size of the xattr field
+ * @param name          the name of the xattr field
+ * @param valuebuffer   the contents of the xattr field
+ * @param valuesize     the size of the xattr field
  * @param stream        where to write the json object
  *
  * @return              0 on success, -1 on error
